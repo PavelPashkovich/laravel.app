0d53856cd4d6adfd4dec21274402fe37732fb199
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2021-12-05 20:31:40 +0300

    First commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,5 @@
+<h1>Hello, LARAVEL!</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at consequuntur, debitis dolore dolorum ducimus ea et eum ipsam magnam, necessitatibus nemo neque nostrum perferendis praesentium quam quod repellendus veritatis. Ad aperiam et impedit iure modi molestiae quo sunt, vitae voluptas voluptatum. Animi commodi cumque fugiat ipsa maxime modi pariatur possimus quis reprehenderit sint! Corporis doloribus ex excepturi fugit impedit incidunt, itaque minus natus nobis praesentium quam qui quos soluta tempora veritatis voluptate voluptatum. Ad eligendi molestiae officia quaerat repudiandae!</p>
+<p></p>
+<p></p>
+<p></p>
